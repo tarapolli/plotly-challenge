1085c180cbfd2bbe30d772323a2cf7bd157e30aa
Tara <tara_polli@yahoo.com>
2021-04-08 12:39:06 -0400

    final app

--- static/js/app.js
+++ static/js/app.js
@@ -4,7 +4,7 @@ var bellybuttonData;
 function init(){
     // populate the dropdown
     var dropdownMenu = d3.select("#selDataset");
-   
+   // Fetch the JSON data
     d3.json("data/samples.json").then((data) =>{
         bellybuttonData = data;
         var subjectID = data.names;
@@ -15,10 +15,12 @@ function init(){
         const selectID = subjectID[0];
         buildPlot(selectID);
         updateMetadata(selectID);
+        // console.log(subjectID);
     });
 }
-// console.log(subjectID);
+
 function buildPlot(sample) {    
+    // Grab values from the json object to build the plots
     d3.json("data/samples.json").then((data) => {
     var samples = data.samples;
     // var personData = data.samples;
@@ -45,86 +47,65 @@ function buildPlot(sample) {
         }
     };
 
-// get the op 10 OTUs found in each individual
-    // var otu_10 = otu_ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse(),
-    // console.log(otu_ids);
-    // // get corresponding top 10 labels for each OTU
-    // var labels = otu_labels.slice(0,10).reverse();
-    // var otu_10 = otu_ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse();
-
-    // -----------------
-    // Getting the top 10 
+    // get the top 10 OTUs in each individual
     var sample_values_10 = result.sample_values.slice(0, 10).reverse();
-    console.log("top 10 sample: " + sample_values_10);
-
-    // // get only top 10 otu ids for the plot OTU and reversing it. 
+    // console.log(sample_values_10);
+    // get only top 10 otu ids for the plot OTU and reversing it. 
     var OTU = (result.otu_ids.slice(0, 10)).reverse();
- 
-    // // get the otu id's to the desired form for the plot
+    // get the otu id's to the desired form for the plot
     var OTU_id = OTU.map(d => "OTU " + d)
-    console.log("OTU IDS: " + OTU_id);
-
-    // // get the top 10 labels for the plot and reversing it.
+    // console.log(OTU_id);
+    //  get corresponding top 10 labels for each OTU
     var labels = result.otu_labels.slice(0, 10).reverse();
-    console.log("labels: " + labels);
+    // console.log(labels);
 
-
-
+    // create data variable for trace layout
     var data = [trace1];
     var layout = {
-        title: 'Bacteria Cultures per Sample',
         showlegend: false,
+        autorange: true,
         hovermode: 'closest',
-        xaxis: {title:"OTU (Operational Taxonomic Unit) ID " +sample},
-        font: { color: "#49a81d", family: "Arial, Helvetica, sans-serif" },
-        margin: {t:30}
+        xaxis: {title:"OTU ID"},
+        margin: {t:20}
     };
     // passing in graph data to render bubble plot
     Plotly.newPlot('bubble', data, layout); 
     var trace1 = {
-        // // x: sample_values.slice(0,10).reverse(),
         x: sample_values_10,
-        // y: otu_ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse(),
         y: OTU_id,
-        // text: otu_labels.slice(0,10).reverse(),
         text: labels,
-        name: "Sample Detail",
         type: "bar",
         orientation: "h"
     };
+    // create data variable for bar chart
     var data = [trace1];
     var layout = {
-        title: "Top Ten OTUs for Individual " +sample,
-        margin: {l: 100, r: 100, t: 100, b: 100},
-        font: { color: "#49a81d", family: "Arial, Helvetica, sans-serif" }
     };
+    // passing in graph data to render bar plot
     Plotly.newPlot("bar", data, layout);  
     });
   }
-  
+  // function to obtain metadata
   function updateMetadata(sample) {
     d3.json("data/samples.json").then((data) => {
+        // obtain Demographic Info for panel
         var metadata = data.metadata;
-        // var personMetadata = data.metadata;
         var filterData = metadata.filter(sampleObject => sampleObject.id == sample);
-        // var filterData = metadata.filter(sampleObject => sampleObject.id == personData);
-
+        // console.log(filterData);
         var result = filterData[0];
+        // console.log(result);
         var panelBody = d3.select("#sample-metadata");
+
          // remove any children from the list before refreshing with new data
          panelBody.html("");
 
-        // Object.entries(result).forEach(([key, value]) => {
-        //     metaPanel.append("h6").text(`${key.toUpperCase()}: ${value}`)
+        // append Demographic Info for panel
         Object.entries(result).forEach((key)=>{
             panelBody.append("p").text(key[0] + ":" + key[1]);
-
         })
-    
-
     });
   }
-  
+  // fresh; allow user to select new Test Subject ID no.
   function optionChanged(selectNew) {
     updateMetadata(selectNew);
     buildPlot(selectNew);
